5f44c7fe39fd5b721b140523cb638e48ed20b3d4
Zach-Allen <30992058+Zach-Allen@users.noreply.github.com>
2020-11-12 20:37:23 -0700

    Items page changes
    Made some UI changes and stubbed out each needed method and class attribute

--- Final_Project/Items/wndItems.xaml.cs
+++ Final_Project/Items/wndItems.xaml.cs
@@ -19,24 +19,57 @@ namespace Final_Project
     /// </summary>
     public partial class wndItems : Window
     {
+        #region Attributes
+        /// <summary>
+        /// This is a boolean to tell the Main 
+        /// Page whether or not it needs to refresh.
+        /// </summary>
+        public bool changesMade;
+
+        /// <summary>
+        /// This will hold a list of items passed in
+        /// from the database
+        /// </summary>
+        public List<DataGrid> items;
+        #endregion
+
         public wndItems()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Button to Add an item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void addItemButton_Click(object sender, RoutedEventArgs e)
         {
-
+            ///When this button is clicked it will check to see if
+            ///anything is selected in the itemsComboBox and if there is it will
+            ///add that item to the invoice and add the item to the list of 
+            ///items in the grid below for the user to see
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void editButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Button to delete an item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void deleteButton_Click(object sender, RoutedEventArgs e)
         {
-
+            ///When an item in the items list is selected and this button
+            ///is clicked it will delete that item from the list
         }
     }
 }
